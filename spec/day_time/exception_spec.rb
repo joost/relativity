13fd9768647fde9e533ec60603ef29b097ba47c2
Peter Vandenabeele <peter@vandenabeele.com>
2012-01-17 23:44:47 +0100

    Revert to seconds_since_midnight ("start" was arbitrary !)

--- spec/day_time/exception_spec.rb
+++ spec/day_time/exception_spec.rb
@@ -8,7 +8,7 @@ describe Relativity::InternalError do
 
   it "raised for hours out of bound" do
     day_time = DayTime.new
-    day_time.instance_variable_set(:@seconds_since_start, -100_000)
+    day_time.instance_variable_set(:@seconds_since_midnight, -100_000)
     lambda { day_time.hours }.should raise_error(Relativity::InternalError, 'hours is out of bound : -28')
   end
 
