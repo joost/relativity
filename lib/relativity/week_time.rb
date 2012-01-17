40885563b03ea11a031549fbd72dcc65e37bf705
Peter Vandenabeele <peter@vandenabeele.com>
2012-01-18 00:53:20 +0100

    WeekTime#new builds a WeekTime close to now

--- lib/relativity/week_time.rb
+++ lib/relativity/week_time.rb
@@ -1,5 +1,29 @@
 class WeekTime
 
+  require 'bigdecimal'
+  require 'time'
+
   attr_reader :seconds_since_sunday_midnight
+
+  def initialize(week_day_string=nil, hours = nil, minutes = nil, seconds = nil, nano_seconds = nil)
+    super()
+    if week_day_string
+#      week_day_d =
+#      hh = hours.to_i
+#      mm = minutes.to_i
+#      ss = seconds.to_i
+#      nn = nano_seconds.to_i
+    else
+      t = Time.new
+      week_day_d = t.wday
+      hh = t.hour
+      mm = t.min
+      ss = t.sec
+      nn = t.nsec
+    end
+
+    seconds_since_midnight = ((hh * 3600) + (mm * 60) + ss + BigDecimal(nn)/BigDecimal(1000000000))%(24*3600)
+    @seconds_since_sunday_midnight = week_day_d * 24 * 3600 + seconds_since_midnight
+  end
 
 end
--- spec/week_time/week_time_spec.rb
+++ spec/week_time/week_time_spec.rb
@@ -10,4 +10,14 @@ describe WeekTime do
     lambda { subject.seconds_since_sunday_midnight }.should_not raise_error
   end
 
+  it "new creates a WeekTime close to now" do
+    wt_s = subject.seconds_since_sunday_midnight
+    t = Time.new
+    day_time_s = (t.hour*60 + t.min)*60 + t.sec + t.nsec/1E9
+    week_day_d = t.wday #0..6 0 is Sunday
+    week_day_s = week_day_d * 24 * 3600
+    week_time_s = day_time_s + week_day_s
+    (((week_time_s - wt_s).abs+2)%(7*24*3600)).should be <= 4
+  end
+
 end
