0504973f6a83ef6c4c4e3e040c7e0887bcc15e66
Peter Vandenabeele <peter@vandenabeele.com>
2012-02-06 18:35:33 +0100

    Add a test for non-equality (by 1 nanosecond)

--- spec/day_time/comparison_spec.rb
+++ spec/day_time/comparison_spec.rb
@@ -8,6 +8,12 @@ describe DayTime do
     dt_1.should == dt_2
   end
 
+  it "== reports difference when different" do
+    dt_1 = DayTime.new(8)
+    dt_2 = DayTime.new(8, 0, 0, 1) # 1 nanosecond off
+    dt_1.should_not == dt_2
+  end
+
   it "== reports exact equality for all arguments" do
     dt_1 = DayTime.new(0,23,45,457834889)
     dt_2 = DayTime.new(0,23,45,457834889)
